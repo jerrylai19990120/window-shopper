3a04ee3e5e823e352ede80afc03444d059f4e08e
jerry <jerrylai19990120@gmail.com>
2021-01-25 18:06:50 -0500

    Initial Commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Jerry Lai on 2021-01-25.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
